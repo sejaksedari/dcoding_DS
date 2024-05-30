f6c21cc17ff57ad290ef5f54888c9b9a9d9245a5
Sejaksedari_Laptop <azaimfuad216@gmail.com>
2024-05-30 19:52:25 +0700

    update

---
+++ Latihan/dcoding_hello.py
@@ -0,0 +1,8 @@
+
+# Modul (dcoding_hello.py) ini akan dipanggil oleh dcoding_main.py dan fungsi serta variable di dalamnya akan dipanggil di sana
+
+def mencari_luas_persegi_panjang(panjang, lebar):
+    luas_persegi_panjang = panjang*lebar
+    return luas_persegi_panjang
+
+nama = "Dicoding Indonesia"
